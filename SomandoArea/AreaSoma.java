43c404db672646ddf9f19747d7d67497edc71360
pirokitaz <nataabiel204@gmail.com>
2026-02-16 21:45:12 -0300

    feature:Area Soma

---
+++ SomandoArea/AreaSoma.java
@@ -0,0 +1,9 @@
+package SomandoArea;
+
+public class AreaSoma {
+    public static void main(String[] args) {
+        double X = 50;
+        double area = (X * X) * 3.14159;
+        System.out.printf("Formato da área é %.4f",area);
+    }
+}
